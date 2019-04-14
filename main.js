b3d5d6d3b8b21f2d1e0a13046312fb786274bb48
Brandy Mello <brandyamello@gmail.com>
2019-04-14 13:34:09 -0600

    Got card to post with title

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const lists = [];
 
 const listItems = document.querySelector('.generated-list-left')
 var taskItem = document.querySelector('#task-item')
-
+var makeListBtn = document.querySelector('.make-task-card-btn')
+var taskTitle = document.querySelector('.task-title')
+var taskBoard = document.querySelector('.right-side')
 // const addTaskBtn = document.querySelector('add-task-image');
 
 addTask.addEventListener('click', addToList)
@@ -12,9 +14,12 @@ addTask.addEventListener('click', addToList)
 
 function addToList(e) {
   e.preventDefault();
+  if(taskItem.value === "") {
+    return;
+  }
   tasks.innerHTML +=
     `
-    <li class="inserted-list-item">
+    <li class="inserted-list-item" id="inserted-list-item">
       <img src="images/delete.svg" class="delete-btn">
       <p>${taskItem.value}</p>
     </li>`
@@ -26,41 +31,37 @@ function clearTaskInput() {
   taskForm.reset();
 }
 
+tasks.addEventListener('click', function (e) {
+  if(e.target.className === 'delete-btn') {
+    e.target.closest('.inserted-list-item').remove();
+  }
+});
 
-
-  // const text = (document.querySelector('.task-item-input')).innerContent;
-  // const task = {
-  //   info: 'info',
-  //   done: false,
-  //   // id,
-  // };
-
-  
-  // const text = (this.querySelector('.task-item'));
-  
-  // this.reset();
-  
-// }
-
+makeListBtn.addEventListener('click', function(e) {
+  taskBoard.insertAdjacentHTML('afterbegin',`
+    <article class="task-list-cards">
+      <div class="card-title">
+        <h2>${taskTitle.value}</h2>
+      </div>
+      <div class="card-list">
+        <ul class="generated-task-list">
+        ${taskItem.value}
+        </ul>
+      </div>
+      <div class="card-navigation">
+        <img src="images/urgent.svg" alt="mark-urgent-button" class="mark-urgent-btn">
+        <label class="mark-urgent-label">Urgent</label>
+        <img src="images/delete.svg" alt="delete-list-button" class="delete-list-btn">
+        <label class="delete-list-btn-label">Delete</label>
+      </div>    
+    </article> `
+);
+})
 
 
 console.log("hello");
-  // if(listItems.value === "") {
-  //   return
-  // } else {
-  //   listItems.innerHTML +=
-  //   `
-  //   <img src="delete.svg">
-  //   <ul>
-  //       <li> ${innerContent.document.querySelector('.task-item').value}
-  //       </li>
-  //     </ul>`
-  // }
+ 
 
-// taskList.addEventListener('submit', addToList);
-// addTask.addEventListener('click', )
-
-// addTask.addEventListener('submit', addTask);
 
 //-------------Insert Card-----------------------------//
 // var makeTaskBtn = document.querySelector('make-task-card-btn');
